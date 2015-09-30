62d6d1e58846212da3893752d0e158dbf67c655f
andbet39 <andrea.terzani@gmail.com>
2015-09-30 17:21:41 +0200

    chat

--- common/models/message.js
+++ common/models/message.js
@@ -6,7 +6,7 @@ module.exports = function(Message) {
 		console.log('sendMessage');
 	    Message.create({'content':content,'posted_at':new Date()},
 	      function(err,mess){
-	      	Message.app.io.emit('message',mess);
+	     	 	Message.app.io.emit('message',mess);
 	    	cb();
 	    });
 	   };
@@ -19,7 +19,45 @@ module.exports = function(Message) {
 	    http: {path:'/sendmessage', verb: 'post'}
 	  });
 
-	
+	Message.sendmessagenew = function(message,cb){
+		 Message.create({'content':message.content,
+		 				 'userId':message.user_id,
+		 				 'roomId':message.room_id,
+		 				 'posted_at':new Date()},
+	      function(err,mess){
+	      		Message.findById(mess.id,{
+  									include: {
+    								relation: 'user'}},function(err,data){
+    									console.log(data);
+    									Message.app.io.emit('message',data);
+	    							cb();
+    								});
+	     	 	
+	    });
+	}
+
+	Message.remoteMethod('sendmessagenew', {
+		accepts: [
+	      {arg: 'message', type: 'object', http: { source: 'body' }}
+	     ],
+	    returns: {arg: 'success', type: 'boolean'},
+	    http: {path:'/sendmessagenew', verb: 'post'}
+	  });
+
+
+	 Message.getmessages = function(cb) {
+	 	console.log('getMessage function');
+	 	Message.find({include: 'user'},function(err,data) {
+	 		//console.log(data);
+      		cb(null,data);
+   		});
+     };
+
+    Message.remoteMethod('getmessages', {
+		accepts: [],
+	    returns: {arg: 'messages', type: 'object'},
+	    http: {path:'/getmessages', verb: 'get'}
+	  });
 
 };
 
